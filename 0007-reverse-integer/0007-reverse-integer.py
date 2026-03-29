af7f373f751cfb0cfd611d9a00cac70fdbd07bc2
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-03-28 22:30:51 -0700

    Time: 39 ms (90.1%), Space: 19.2 MB (83.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,15 +1,12 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        x_copy = x
+        sign = -1 if x < 0 else 1
         x = abs(x)
         rev = 0
-        while(x>0):
-            last = x%10
-            rev=rev*10+last
-            x=x//10
-
-        limit = 2**31
-        if rev < -limit or rev >limit:
-            return 0
-
-        return -rev if x_copy<0 else rev
+        LIMIT = 2**31
+        while x:
+            x, mod = divmod(x, 10)
+            rev = rev * 10 + mod
+            if rev > LIMIT - 1:
+                return 0
+        return sign * rev
