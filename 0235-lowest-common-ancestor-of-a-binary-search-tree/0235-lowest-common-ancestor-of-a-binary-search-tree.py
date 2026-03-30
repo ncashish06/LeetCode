bce6ee8df86ed3d963206dea7ca6672acc867f9b
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-03-30 13:36:38 -0700

    Time: 56 ms (93.02%), Space: 21.8 MB (94.61%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,38 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+        # Solution for Binary Tree, LeetCode Problem 236
+        # My approach 
+        lca = None
+        # bottom-up recursion, go till null node (child of leaf node) in the tree and then traverse back up
+        def traverse (curr_node):
+            nonlocal lca
+            count = 0
+            if not curr_node:
+                return 0
+            is_node_on_left = traverse(curr_node.left)
+            is_node_on_right = traverse(curr_node.right)
+            if curr_node.val == p.val or curr_node.val == q.val:
+                count+=1
+            count = count + is_node_on_left + is_node_on_right
+            if count == 2 and not lca: # skip updating lca as you traverse back up the tree after you have found lca
+                lca = curr_node # setting lca here
+
+            return count
+
+        traverse(root)
+        return lca
+        """
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
