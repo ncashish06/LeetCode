e6bf01862e753c7346a6bff7ea51da81910e9555
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-03-30 18:16:40 -0700

    Time: 16 ms (87.22%), Space: 25.9 MB (95.44%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # If total cost (sum of costs) is greater than the available gas, then we can't complete the circuit
+        # Rule is if at all the total sum of gains is negative, we don't have a solution
+        total_gas_available = sum(gas)
+        total_gas_cost_to_travel = sum(cost)
+        if total_gas_cost_to_travel > total_gas_available:
+            return -1
+        
+        curr_gain = 0
+        start_point = 0
+        for i in range(len(gas)):
+            gain = gas[i]-cost[i]
+            curr_gain+=gain
+            if curr_gain < 0:
+                start_point = i+1
+                curr_gain = 0
+
+        return start_point if curr_gain >=0 else -1
