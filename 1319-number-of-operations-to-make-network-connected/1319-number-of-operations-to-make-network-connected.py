edab3c57461e5ed363acdb5736b4534f21e30911
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-03-31 15:13:37 -0700

    Time: 27 ms (93.85%), Space: 33.7 MB (46.93%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,32 @@
+from collections import deque
+class Solution:
+    # solution is number of unconnected components (think like islands) - 1
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        if len(connections) < n-1:
+            return -1
+        
+        graph = [[] for _ in range(n)]
+        for frm, to in connections:
+            graph[frm].append(to)
+            graph[to].append(frm)
+
+        no_of_unconnected_components = 0
+        visited = [False]*n
+
+        def bfs(src):
+            q = deque([src])
+            visited[src]=True
+            while len(q):
+                curr_node = q.popleft()
+                for neighbor in graph[curr_node]:
+                    if not visited[neighbor]:
+                        visited[neighbor] = True
+                        q.append(neighbor)
+
+        for i in range(n):
+            if not visited[i]:
+                no_of_unconnected_components+=1
+                bfs(i)
+
+        return no_of_unconnected_components-1
+
