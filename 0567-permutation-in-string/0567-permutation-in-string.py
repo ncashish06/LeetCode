193138c28981a3ef4b92ce98d64c077b0445cde7
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-03-31 20:44:50 -0700

    Time: 15 ms (82.3%), Space: 19.4 MB (70.47%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,25 +2,25 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        hash_s = [0]*26
-        hash_w = [0]*26
+        hash_s1 = [0]*26
+        hash_window = [0]*26
         window_length = len(s1)
 
         def is_hash_same(h1, h2):
             return h1 == h2
 
         for i in range(window_length):
-            hash_s[ord(s1[i])-ord('a')]+=1
-            hash_w[ord(s2[i])-ord('a')]+=1
+            hash_s1[ord(s1[i])-ord('a')]+=1
+            hash_window[ord(s2[i])-ord('a')]+=1
 
         i=0
         j=window_length-1
         while j<len(s2):
-            if is_hash_same(hash_s, hash_w):
+            if is_hash_same(hash_s1, hash_window):
                 return True
-            hash_w[ord(s2[i])-ord('a')]-=1
+            hash_window[ord(s2[i])-ord('a')]-=1 #if hash not matched then revert hash_window to all 0
             i+=1
             j+=1
             if j<len(s2):
-                hash_w[ord(s2[j])-ord('a')]+=1
+                hash_window[ord(s2[j])-ord('a')]+=1 #update window hash with next set of characters
         return False
