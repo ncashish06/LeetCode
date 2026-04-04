6a5925f79390c59ef5d1333ff75b31ade750647e
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-03 19:35:59 -0700

    Time: 2 ms (53.73%), Space: 23.3 MB (88.53%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -10,3 +10,12 @@ class Solution:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
+        # Recursive way: in-place but not constant space (O(n) for the stack) as above
+        """
+        def helper(left, right):
+            if left < right:
+                s[left], s[right] = s[right], s[left]
+                helper(left + 1, right - 1)
+
+        helper(0, len(s) - 1)
+        """
