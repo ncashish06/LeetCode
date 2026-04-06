99579a4fbe32b5f9d401e31676e7353c8eda3b82
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-05 23:51:34 -0700

    Time: 323 ms (21.84%), Space: 27.5 MB (90.85%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.py
@@ -1,5 +1,7 @@
 class Solution:
     def decodeCiphertext(self, encodedText: str, rows: int) -> str:
+        # think length of encodedText as area = rows*col
+        """
         if rows == 1:
             return encodedText
         n = len(encodedText)
@@ -13,5 +15,16 @@ class Solution:
                 r += 1
                 c += 1
         return "".join(res).rstrip()
+        """
+        original_text = ""
+        cols = len(encodedText)//rows
+
+        for i in range(cols):
+            next_pos = i
+            while next_pos < len(encodedText):
+                original_text += encodedText[next_pos]
+                next_pos += cols + 1 #Jump to next_poss
+
+        return original_text.rstrip()
         
         
