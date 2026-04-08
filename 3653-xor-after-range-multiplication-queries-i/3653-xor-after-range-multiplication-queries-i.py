566fa7cec4977702308af7582fcc3edd221dae69
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-08 00:02:29 -0700

    Time: 1495 ms (97.22%), Space: 20 MB (79.94%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -1,14 +1,19 @@
 class Solution:
+    # Date Solved: 7 April 2026, Tuesday
+    # Time: O(nq) where n is the length of nums, and q is the number of queries.
+    # Space: O(1)
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
         res = 0
+        MOD = 10**9 + 7
         for q in range(len(queries)):
             l, r, k, v = queries[q]
             idx = l
-            while idx <= r:
-                nums[idx] = (nums[idx] * v) % (10**9 + 7)
-                idx+=k
-        
+            for idx in range(l, r + 1, k):  # for loop faster than while loop
+                # while idx <=r:
+                nums[idx] = (nums[idx] * v) % MOD
+                # idx+=k
+
         for i in range(len(nums)):
-            res^=nums[i]
+            res ^= nums[i]
 
         return res
