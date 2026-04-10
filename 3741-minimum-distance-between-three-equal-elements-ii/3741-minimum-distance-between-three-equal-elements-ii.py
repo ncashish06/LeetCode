79e88cfe4402183e4517ec245163e494b1d59adc
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-09 20:26:46 -0700

    Time: 337 ms (66.13%), Space: 49.2 MB (84.41%) - LeetHub

---
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.py
@@ -0,0 +1,26 @@
+class Solution:
+    # Date Solved: 9 April 2026, Thursday
+    def minimumDistance(self, nums: List[int]) -> int:
+        indices_map = {}
+        n = len(nums)
+        for idx, num in enumerate(nums):
+            if num not in indices_map:
+                indices_map[num] = [idx]
+            else:
+                indices_map[num].append(idx)
+
+        min_total_dist = float("inf")
+        window_size = 3
+
+        for num, indices in indices_map.items():
+            if len(indices) < 3:
+                continue
+
+            for i in range(len(indices) - window_size + 1):  # Standard Sliding Window "Start-Point" Template
+                first_idx = indices[i]
+                third_idx = indices[i + 2]
+                # In sorted triplet i<j<k, the distance is (j-i)+(k-j)+(k-i),simplifies to 2k-2i.
+                current_dist = 2 * (third_idx - first_idx)
+                min_total_dist = min(min_total_dist, current_dist)
+
+        return min_total_dist if min_total_dist != float("inf") else -1
