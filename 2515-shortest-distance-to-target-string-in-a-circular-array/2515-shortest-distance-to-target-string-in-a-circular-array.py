721662430c70637cf79e6cf9eadf603e60d52b13
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-15 00:12:25 -0700

    Time: 0 ms (100%), Space: 19.3 MB (91.4%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.py
@@ -1,9 +1,10 @@
 class Solution:
     # Date Solved: 14 April 2026, Tuesday
     # Time: O(nL), L = length of word
+    # Related: https://leetcode.com/problems/minimum-time-to-type-word-using-special-typewriter/
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
         ans = n = len(words)
         for i, word in enumerate(words):
             if word == target:
-                ans = min(ans, abs(i - startIndex), n - abs(i - startIndex))
+                ans = min(ans, abs(i - startIndex), n - abs(i - startIndex)) # Clockwise vs Counter-clockwise
         return ans if ans < n else -1
