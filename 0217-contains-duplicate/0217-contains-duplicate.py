06bd10ec6ddebedf1dc96aabfc70cdc55e221445
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-20 01:01:14 -0700

    Time: 11 ms (65.68%), Space: 31.1 MB (86.98%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -2,9 +2,9 @@ class Solution:
     # Date Solved: 20 April 2026, Monday
     # Blind 75
     def containsDuplicate(self, nums: List[int]) -> bool:
-        last_seen = {}
-        for i, val in enumerate(nums):
-            if val in last_seen:
+        last_seen = set()
+        for num in nums:
+            if num in last_seen:
                 return True
-            last_seen[val] = i
+            last_seen.add(num)
         return False
