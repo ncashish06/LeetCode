566df865a6d1c589c8cea15fa0b9fa3e36d37799
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-22 21:05:37 -0700

    Time: 139 ms (59.45%), Space: 55.8 MB (44.49%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.py
+++ 2615-sum-of-distances/2615-sum-of-distances.py
@@ -11,22 +11,17 @@ class Solution:
         for indices in indices_map.values():
             total_sum = sum(indices)
             prefix_sum = 0
-            m = len(indices)
-            
+
             for i, actual_idx in enumerate(indices):
                 # i is the count of elements to the left
-                # m - i - 1 is the count of elements to the right
-                
-                # Formula derived from your image:
-                # Left part: (i * actual_idx) - prefix_sum
-                # Right part: (total_sum - prefix_sum - actual_idx) - ((m - i - 1) * actual_idx)
-                
+                # len(indices) - i - 1 is the count of elements to the right
+
                 left_side = (i * actual_idx) - prefix_sum
-                right_side = (total_sum - prefix_sum - actual_idx) - ((m - i - 1) * actual_idx)
-                
+                right_side = (total_sum - prefix_sum - actual_idx) - (
+                    (len(indices) - i - 1) * actual_idx
+                )
+
                 res[actual_idx] = left_side + right_side
-                
-                # Update prefix_sum for the next element in this group
                 prefix_sum += actual_idx
-                
+
         return res
