6e3c3419d665afae15a896d259a254e1342d41ab
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-23 15:41:04 -0700

    Time: 3 ms (88.54%), Space: 23 MB (32.51%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,17 @@
 import heapq
 from collections import Counter
 
+
 class Solution:
+    # Date Solved: 23 April 2026, Thursday
+    # Refer: Namaste DSA.
+    # Intuition: Store elements frequency in Hash Map. Then you can sort based on the values and return top K but this approach would take time of O(nlogn). If we use heap here, we can reduce time to O(nlogk).
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = Counter(nums)
+        freq_map = Counter(nums)  # Time: O(n)
         min_heap = []
         for num, freq in freq_map.items():
-            heapq.heappush(min_heap, (freq, num))
+            heapq.heappush(min_heap, (freq, num))  # Time: O(logk)
             if len(min_heap) > k:
                 heapq.heappop(min_heap)
-        
+
         return [num for freq, num in min_heap]
