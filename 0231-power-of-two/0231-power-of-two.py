c23efa272288bf843229757cd9df725cef7fd046
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-26 13:30:26 -0700

    Time: 0 ms (100%), Space: 19.2 MB (55.63%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -2,17 +2,20 @@ class Solution:
     # Date Solved: 13 April 2026, Monday
     def isPowerOfTwo(self, n: int) -> bool:
         """
+        # Top down recursion
         if n == 1:
             return True
         elif n < 1 or n % 2 != 0:
             return False
         return self.isPowerOfTwo(n // 2)
         """
-        # Bitwise AND operator
+        """
+        # Bitwise AND operator: Get/Isolate the Rightmost 1-bit
         # if n <= 0:
         #     return False
         # return n & (-n) == n
-
+        """
+        # Bitwise AND operator: Turn off the Rightmost 1-bit
         if n <= 0:
             return False
         return n & (n - 1) == 0
