c97158409856bdad135d3b14e2b9f9515baacf27
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-04-28 20:53:46 -0700

    Time: 0 ms (100%), Space: 20.6 MB (36.32%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,10 +1,16 @@
+import bisect
+
+
 class Solution:
     # Date Solved: 28 April 2026, Tuesday
     def search(self, nums: List[int], target: int) -> int:
+        """
+        # Iterative binary search
         left = 0
         right = len(nums) - 1
         while left <= right:
-            middle = (left + right) // 2
+            # middle = middle = (left + right) // 2
+            middle = left + ((right - left) // 2)  # Overflow safe version of above
             if nums[middle] == target:
                 return middle
             elif target < nums[middle]:
@@ -12,3 +18,17 @@ class Solution:
             else:
                 left = middle + 1
         return -1
+
+        # Upper bound
+        idx = bisect.bisect_right(nums, target)
+        if idx > 0 and nums[idx - 1] == target:
+            return idx - 1
+        else:
+            return -1
+        """
+        # Lower bound
+        idx = bisect.bisect_left(nums, target)
+        if idx < len(nums) and nums[idx] == target:
+            return idx
+        else:
+            return -1
