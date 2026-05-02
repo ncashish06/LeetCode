7a3e6ad24ba3b122ca53595f6864bfbfa6ab8670
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-02 00:33:40 -0700

    Time: 48 ms (23.79%), Space: 19.3 MB (37.92%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotatedDigits(self, n: int) -> int:
+        ans = 0
+        # For each x in [1, N], check whether it's good
+        for x in range(1, n+1):
+            S = str(x)
+            # Each x has only rotateable digits, and one of them
+            # rotates to a different digit
+            ans += (all(d not in '347' for d in S)
+                    and any(d in '2569' for d in S))
+        return ans
