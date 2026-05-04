807f792ceb176d6c0fd644b623f4bd13726ef8ac
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-03 21:06:36 -0700

    Time: 0 ms (100%), Space: 19.5 MB (5.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,20 +1,40 @@
 class Solution:
+    # This is Blind 75 question from Neetcode
+    # Check Neetcode for solution explanation and video
     def rotate(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix)
+        """
+        # Approach 1: Rotate By Four Cells
+        l = 0
+        r = len(matrix[0]) - 1  # right = number of columns - 1
+        while l < r:
+            for i in range(r - l):
+                top, bottom = l, r
 
-        for i in range(n // 2):
-            for j in range((n + 1) // 2):
-                (
-                    matrix[i][j],
-                    matrix[n - 1 - j][i],
-                    matrix[n - 1 - i][n - 1 - j],
-                    matrix[j][n - 1 - i],
-                ) = (
-                    matrix[n - 1 - j][i],
-                    matrix[n - 1 - i][n - 1 - j],
-                    matrix[j][n - 1 - i],
-                    matrix[i][j],
-                )
+                # save the topleft
+                topLeft = matrix[top][l + i]
+
+                # move bottom left into top left
+                matrix[top][l + i] = matrix[bottom - i][l]
+
+                # move bottom right into bottom left
+                matrix[bottom - i][l] = matrix[bottom][r - i]
+
+                # move top right into bottom right
+                matrix[bottom][r - i] = matrix[top + i][r]
+
+                # move top left into top right
+                matrix[top + i][r] = topLeft
+            r -= 1
+            l += 1
+        """
+        # Approach 2: Reverse And Transpose
+        # Reverse the matrix vertically
+        matrix.reverse()
+
+        # Transpose the matrix
+        for i in range(len(matrix)):
+            for j in range(i + 1, len(matrix)):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
