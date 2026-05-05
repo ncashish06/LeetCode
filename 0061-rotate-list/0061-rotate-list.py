17932f247c825d12aae2c652c745e9f5a75a91d0
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-04 20:20:50 -0700

    Time: 0 ms (100%), Space: 19.2 MB (95%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -5,28 +5,23 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # base cases
         if not head:
-            return None
-        if not head.next:
             return head
 
-        # close the linked list into the ring
-        old_tail = head
-        n = 1
-        while old_tail.next:
-            old_tail = old_tail.next
-            n += 1
-        old_tail.next = head
+        length, tail = 1, head
+        while tail.next:
+            tail = tail.next
+            length += 1
 
-        # find new tail : (n - k % n - 1)th node
-        # and new head : (n - k % n)th node
-        new_tail = head
-        for i in range(n - k % n - 1):
-            new_tail = new_tail.next
-        new_head = new_tail.next
+        k = k % length
+        if k == 0:
+            return head
 
-        # break the ring
-        new_tail.next = None
+        cur = head
+        for i in range(length - k - 1):
+            cur = cur.next
+        newHead = cur.next
+        cur.next = None
+        tail.next = head
 
-        return new_head
+        return newHead
