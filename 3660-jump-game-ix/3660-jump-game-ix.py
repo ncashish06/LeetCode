9544d562d5c5be8fb2eb7cada8eba56333d8b8a4
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-06 20:55:52 -0700

    Time: 219 ms (69.83%), Space: 41.1 MB (27.59%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxValue(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [0] * n
+        stack = []  # [(max_val, left_idx, right_idx)]
+        for i in range(n):
+            curr_max = nums[i]
+            curr_left = i
+            while stack and stack[-1][0] > nums[i]:
+                top_max, top_left, top_right = stack.pop()
+                curr_max = max(curr_max, top_max)
+                curr_left = top_left
+            stack.append((curr_max, curr_left, i))
+
+        for max_val, left_idx, right_idx in stack:
+            for j in range(left_idx, right_idx + 1):
+                ans[j] = max_val
+        return ans
