67d260406363b490b6f680c9ee74ce6888c2a21e
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-13 23:56:21 -0700

    Time: 0 ms (100%), Space: 19.4 MB (17.79%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        maxVal = max(nums)
+        if len(nums) < maxVal + 1:
+            return False
+        nums.sort()
+        curr = 1
+        for i in range(len(nums) - 1):
+            if nums[i] != i + 1:
+                return False
+        return nums[-1] == nums[-2]
