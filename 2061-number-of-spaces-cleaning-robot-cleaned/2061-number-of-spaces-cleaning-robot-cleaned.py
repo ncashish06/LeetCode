96e65a010076ee22c9805a09fd6a593f850abb44
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-15 22:20:53 -0700

    Time: 0 ms (100%), Space: 23.9 MB (68.75%) - LeetHub

--- 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.py
+++ 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.py
@@ -1,12 +1,14 @@
 class Solution:
     # Date Solved: 15 May 2026, Friday
     # Weekly Premium April 2026 - Week 3
+    # Don't assume all 0's would be cleaned
+    # Code very similar to LC.54 Spiral Matrix (Blind 75)
     def numberOfCleanRooms(self, room: List[List[int]]) -> int:
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         rows, cols = len(room), len(room[0])
-        cleaned = set()
         visited = set()
+        cleaned = set() # As there will be multiple entries for each cell in the visited set, determining the number of cleaned cells may be challenging. Therefore, we will also maintain a set called cleaned, which solely stores the cell coordinates of each cleaned cell.
 
         row, col, direction = 0, 0, 0  # start at top-left, facing right
 
