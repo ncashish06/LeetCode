fce9e6e6927267281a7f1243d3fc59f6f8fd43d4
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-20 00:16:13 -0700

    Time: 46 ms (90.61%), Space: 55.5 MB (85.14%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -1,6 +1,14 @@
 class Solution:
     # Date Solved: 11 May 2026, Monday, Daily Problem
     def minimumEffort(self, tasks: List[List[int]]) -> int:
+        """
+        Greedy approach: sort tasks by (minimum - actual) ascending.
+        For each task, (minimum - actual) represents the "buffer" it demands from your initial energy beyond what it actually costs.
+        Tasks with a small buffer are  efficient as they don't require much cushion, so do them first while energy is high.
+        Tasks with a large buffer are done last, where the accumulated starting energy can absorb that cushion without inflating the overall minimum.
+
+        Time: O(nlogn), Space: O(logn) due to Python's Timsort (Merge sort recursion)
+        """
         tasks.sort(key=lambda task: task[1] - task[0])
         min_energy_needed = 0
         for actual, minimum in tasks:
