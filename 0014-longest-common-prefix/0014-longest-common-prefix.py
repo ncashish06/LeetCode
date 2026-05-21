2bf29974b8f93a2baa78a6de0591e472599b72c8
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-20 22:28:19 -0700

    Time: 3 ms (22.37%), Space: 19.3 MB (31.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,11 +1,24 @@
 class Solution:
     # Date Solved: 14 April 2026, Tuesday
+    # NeetCode 250
+    # Time: O(n*m), Space: O(1)
+    # Where n is the length of the shortest string and  m is the number of strings.
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        x = 0
-        while x < len(strs[0]):
-            ch = strs[0][x]
-            for i in range(1, len(strs)):
-                if x == len(strs[i]) or strs[i][x] != ch:
-                    return strs[0][:x]
-            x += 1
-        return strs[0]
+        prefix = strs[0]  # Initial Prefix Candidate
+
+        for i in range(1, len(strs)):
+            j = 0
+
+            # Stop if we reach the end of either string or character don't match
+            while j < min(len(prefix), len(strs[i])):
+                if prefix[j] != strs[i][j]:
+                    break
+                j += 1
+
+            # Trim prefix to only the matched portion,
+            # If j=0 (no match), prefix becomes "" and we can stop early
+            prefix = prefix[:j]
+            if not prefix:
+                return ""
+
+        return prefix
