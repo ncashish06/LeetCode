e2c22aa32a0f90d17108f3cd5af2c007706e2e22
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-22 19:17:20 -0700

    Time: 0 ms (100%), Space: 19.2 MB (53.16%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -2,7 +2,7 @@ class Solution:
     # Date Solved: 22 May 2026, Friday, POTD
     # Refer: codestorywithMIK, Neetcode
     def check(self, nums: List[int]) -> bool:
-        # Approach 1: Count Peek/Pivot/Rotation Points (Only 1 peek element/index should be present)
+        # Count Peek/Pivot/Rotation Points (Only 1 peek element/index should be present)
         n = len(nums)
         peak = 0
 
