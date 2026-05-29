247de6ca247f1d96bb4768d9ef52356a6e3e38cd
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-05-29 12:49:55 +0200

    Time: 0 ms (100%), Space: 19.3 MB (61.73%) - LeetHub

---
+++ 1056-confusing-number/1056-confusing-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Date Solved: 29 May 2026, Friday
+    # Refer: LC246. Strobogrammatic Number
+    def confusingNumber(self, n: int) -> bool:
+        inverse_char = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}
+
+        s = str(n)
+
+        # First check all digits are valid (0,1,6,8,9 only)
+        for ch in s:
+            if ch not in inverse_char:
+                return False
+
+        # Build the rotated number (reverse + map each digit)
+        rotated = "".join(inverse_char[ch] for ch in reversed(s))
+
+        return rotated != s
