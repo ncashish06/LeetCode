b3d01ea6d42abbd1f8292a545d438dbb2fedcbce
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-08 13:21:03 +0200

    Time: 68 ms (13.48%), Space: 34 MB (17.69%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,13 +1,42 @@
 class Solution:
+    # Date Solved: 8 June 2026, Monday, POTD
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        less_than_pivot, equal_pivot, greater_than_pivot = [], [], []
+        """
+        # Approach 1 - Using 3 extra lists
+        # Time: O(n), Space: O(n)
+        less, equal, greater = [], [], []
+
         for num in nums:
             if num < pivot:
-                less_than_pivot.append(num)
+                less.append(num)
             elif num == pivot:
-                equal_pivot.append(num)
+                equal.append(num)
             else:
-                greater_than_pivot.append(num)
-        less_than_pivot.extend(equal_pivot)
-        less_than_pivot.extend(greater_than_pivot)
-        return less_than_pivot
+                greater.append(num)
+        less.extend(equal)
+        less.extend(greater)
+        return less
+        # return less + equal + greater
+        """
+        # Approach 2 - Using index pointers
+        # Time: O(n), Space: O(n) for output list
+        count_less = sum(1 for num in nums if num < pivot)
+        count_equal = sum(1 for num in nums if num == pivot)
+
+        i = 0  # index for less than pivot
+        j = count_less  # index for equal to pivot
+        k = count_less + count_equal  # index for greater than pivot
+        result = [0] * len(nums)
+
+        for num in nums:
+            if num < pivot:
+                result[i] = num
+                i += 1
+            elif num == pivot:
+                result[j] = num
+                j += 1
+            else:
+                result[k] = num
+                k += 1
+
+        return result
