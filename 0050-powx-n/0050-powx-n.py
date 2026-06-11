1cc70dbdc3a0abae0204733e6b1909ad1f25465b
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-11 15:13:30 +0200

    Time: 0 ms (100%), Space: 19.6 MB (18.84%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,20 @@
+class Solution:
+    # Date Solved: 11 June 2026, Thursday
+    # NC150
+    # Refer: codestorywithMIK, binary or fast exponentiation
+    # This code used in 11 June 2026 POTD, LC. 3558 Number of Ways to Assign Edge Weights I
+    def myPow(self, x: float, n: int) -> float:
+        def power(x, n):
+            if n == 0:
+                return 1
+            half = power(x, n // 2)
+            res = half * half
+            if n % 2 == 1:
+                res *= x
+            return res
+
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        return power(x, n)
