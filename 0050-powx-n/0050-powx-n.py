080d5846a16210a50f4a213cb3b6b39f6fb3fd62
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-11 15:27:19 +0200

    Time: 3 ms (11.7%), Space: 19.5 MB (54.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,6 +5,8 @@ class Solution:
     # This code used in 11 June 2026 POTD, LC. 3558 Number of Ways to Assign Edge Weights I
     def myPow(self, x: float, n: int) -> float:
         def power(x, n):
+            if x == 0: # To eliminate unnecessary computation when n is large and x is 0
+                return 0
             if n == 0:
                 return 1
             half = power(x, n // 2)
