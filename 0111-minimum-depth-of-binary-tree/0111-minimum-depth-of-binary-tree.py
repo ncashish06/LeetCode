aa3b3f4d5444e3ec99a73a40a6533738117398cd
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-12 18:13:44 +0200

    Time: 72 ms (5.86%), Space: 44.1 MB (98.08%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.py
@@ -4,21 +4,52 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
+
+
 class Solution:
     # Date Solved: 11 June 2026, Thursday
-    # Refer: Alvin The Programmer YouTube
-    # Time: O(N), Space: O(N) when tree is unbalanced
+    # Refer: codestorywithMIK
     def minDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        return self._minDepth(root)
-
-    def _minDepth(self, root: Optional[TreeNode]) -> int:
-        # Bottom up recursion approach
+        # Approach 1: DFS
+        # Time: O(N), Space: O(N) when tree is unbalanced
         if not root:
-            return float("inf")
+            return 0
+
+        if root.left is None and root.right is None:
+            return 1
+
+        left = self.minDepth(root.left) if root.left else float("inf")
+        right = self.minDepth(root.right) if root.right else float("inf")
+
+        return 1 + min(left, right)
+        """
+        # Approach 2: BFS
+        # Time: O(N), Space: O(N)
+        if not root:
+            return 0
+
         if not root.left and not root.right:
             return 1
-        left_min_depth = self._minDepth(root.left)
-        right_min_depth = self._minDepth(root.right)
-        return 1 + min(left_min_depth, right_min_depth)
+
+        queue = deque([root])
+        depth = 1
+
+        while queue:
+            n = len(queue)
+
+            for _ in range(n):
+                temp = queue.popleft()
+
+                if not temp.left and not temp.right:
+                    return depth
+
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+
+            depth += 1
+
+        return -1
+        """
