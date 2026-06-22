46e710ca124c87fe3fad6a7274bb24bcd7d26106
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-22 13:25:29 +0200

    Time: 143 ms (29.52%), Space: 24.7 MB (90.48%) - LeetHub

---
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -0,0 +1,37 @@
+# Definition for Node.
+# class Node:
+#     def __init__(self, val=0, left=None, right=None, random=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#         self.random = random
+from collections import deque
+
+
+class Solution:
+    # Date Solved: 22 June 2026, Monday, Weekly Premium W4
+    # Almost same solution as LC. 133 Clone Graph (Blind 75)
+    # Refer: Namaste DSA (BFS)
+    def copyRandomBinaryTree(self, root: "Optional[Node]") -> "Optional[NodeCopy]":
+        if not root:
+            return None
+
+        visited = {}
+        q = deque([root])
+
+        visited[root] = NodeCopy(root.val)
+
+        while q:
+            curr = q.popleft()
+            cloneCurr = visited[curr]
+
+            for neighbor in [curr.left, curr.right, curr.random]:
+                if neighbor and neighbor not in visited:
+                    visited[neighbor] = NodeCopy(neighbor.val)
+                    q.append(neighbor)
+
+            cloneCurr.left = visited.get(curr.left)
+            cloneCurr.right = visited.get(curr.right)
+            cloneCurr.random = visited.get(curr.random)
+
+        return visited[root]
