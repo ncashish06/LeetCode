1f277e580a4c52fa409a9070487585a484b3af80
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-26 10:30:45 +0200

    Time: 106 ms (63.96%), Space: 40.9 MB (12.61%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.py
@@ -0,0 +1,85 @@
+class Solution:
+    # Date Solved: 25 June 2026, Thursday, POTD
+    # Refer: codestorywithMIK
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        """
+        # Approach 1: Brute force TLE, Time: O(n^3)
+        n = len(nums)
+        res = 0
+
+        for i in range(n):
+            for j in range(i, n):
+                count = 0
+                for k in range(i, j + 1):
+                    if nums[k] == target:
+                        count += 1
+                if count > (j - i + 1) // 2:
+                    res += 1
+        return res
+
+        # Approach 2: Brute force, Time: O(n^2)
+        n = len(nums)
+        res = 0
+
+        for i in range(n):
+            count = 0
+            for j in range(i, n):
+                if nums[j] == target:
+                    count += 1
+                if count > (j - i + 1) // 2:
+                    res += 1
+        return res
+
+        # Approach 3: Optimized Brute force (no division), Time: O(n^2)
+        n = len(nums)
+        res = 0
+
+        for i in range(n):
+            count = 0
+            for j in range(i, n):
+                # Inspired by Boyer-Moore: Same +1/-1 trick to track if target is "winning" but different. Boyer-Moore resets candidate when count hits 0
+                if nums[j] == target:
+                    count += 1
+                else:
+                    count -= 1
+                if count > 0:
+                    res += 1
+        return res
+
+        # Approach 4: Cumulative sum concept introduced, Time: O(n^2)
+        n = len(nums)
+        res = 0
+
+        cumulative_sum = [0] * (n + 1)
+        for i in range(n):
+            cumulative_sum[i + 1] = cumulative_sum[i] + (1 if nums[i] == target else -1)
+
+        for i in range(n):
+            for j in range(i, n):
+                if cumulative_sum[j + 1] - cumulative_sum[i] > 0:
+                    res += 1
+
+        return res
+        """
+        # Approach 5: O(n) solution, Refer: codestorywithMIK
+        from collections import defaultdict
+
+        mp = defaultdict(int)
+        mp[0] = 1
+
+        curr_sum = 0
+        valid_left_points = 0
+        result = 0
+
+        for x in nums:
+            if x == target:
+                valid_left_points += mp[curr_sum]
+                curr_sum += 1
+            else:
+                curr_sum -= 1
+                valid_left_points -= mp[curr_sum]
+
+            mp[curr_sum] += 1
+            result += valid_left_points
+
+        return result
