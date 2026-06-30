b3fbf72172e4aad4b752239ae252b721493ff772
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-30 14:42:21 +0200

    Time: 53 ms (97.47%), Space: 22.7 MB (43.73%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,34 +5,35 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        """
-        # Solution for Binary Tree, LeetCode Problem 236
-        # My approach 
-        lca = None
-        # bottom-up recursion, go till null node (child of leaf node) in the tree and then traverse back up
-        def traverse (curr_node):
-            nonlocal lca
-            count = 0
-            if not curr_node:
-                return 0
-            is_node_on_left = traverse(curr_node.left)
-            is_node_on_right = traverse(curr_node.right)
-            if curr_node.val == p.val or curr_node.val == q.val:
-                count+=1
-            count = count + is_node_on_left + is_node_on_right
-            if count == 2 and not lca: # skip updating lca as you traverse back up the tree after you have found lca
-                lca = curr_node # setting lca here
-
-            return count
-
-        traverse(root)
-        return lca
-        """
+    # Date Solved: 30 June 2026, Tuesday
+    # Refer: Namaste DSA, NeetCode and codestorywithMIK
+    # Blind 75.
+    # LC. 235 LCA of Binary Tree (not in NC250) solution will work for this BST problem as well.
+    # This problem can also be solved using Binary Lifting Technique (Jumping up in the powers of 2 instead of one jump at a time) but is overkill.
+    # Refer: codestorywithMIK Binary Lifting (DP) playlist, 3rd of 4 videos
+    def lowestCommonAncestor(self, root: "TreeNode", p: "TreeNode", q: "TreeNode") -> "TreeNode":
+        # Approach 1: Recursion
+        # Time: O(h) or O(logn) for average case where h = height of the tree.  O(n) worst case : skewed BST.
+        # Space: O(h) or O(logn) for average case where h = height of the tree.  O(n) worst case : skewed BST.
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
         elif p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
         else:
             return root
+        """
+        # Approach 2: Iteration
+        # Time: O(h) or O(logn) for average case where h = height of the tree.  O(n) worst case : skewed BST.
+        # Space: O(1)
+        cur = root
+
+        while cur:
+            if p.val > cur.val and q.val > cur.val:
+                cur = cur.right
+            elif p.val < cur.val and q.val < cur.val:
+                cur = cur.left
+            else:
+                return cur
+        """
