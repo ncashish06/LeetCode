b0a34cb31272f75b8b8c067fc6af04a731584df3
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-06-30 14:14:05 +0200

    Time: 147 ms (8.88%), Space: 50.8 MB (10.58%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -10,6 +10,8 @@ class Solution:
     # Date Solved: 30 June 2026, Tuesday
     # Refer: codestorywithMIK, NeetCode (no video, only solution) and Namaste DSA
     # Not in NC250 but LC. 235 LCA of BST is Blind 75
+    # This problem can also be solved using Binary Lifting Technique (Jumping up in the powers of 2 instead of one jump at a time) but is overkill.
+    # Refer: codestorywithMIK Binary Lifting (DP) playlist, 3rd of 4 videos
     def lowestCommonAncestor(self, root: "TreeNode", p: "TreeNode", q: "TreeNode") -> "TreeNode":
         # Base Case: If we hit a null node or find p or q, return it up
         if root is None or root is p or root is q:
