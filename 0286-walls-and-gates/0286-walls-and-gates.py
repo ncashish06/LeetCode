f70f5589a684811f9b05b973168c286510efb766
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-01 15:12:04 +0200

    Time: 24 ms (96.22%), Space: 22.1 MB (74.6%) - LeetHub

---
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+
+class Solution:
+    # Date Solved: 1 July 2026, Wednesday
+    # NC150
+    # Solved on my own. Multi-source BFS, similar to Rotting Oranges
+    # One component of today's POTD is based on this. Today's POTD LC. 2812 covers multiple topics: BFS, Multi-Source BFS and Binary Search
+    # Time: , Space:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        rows, cols = len(rooms), len(rooms[0])
+        queue = deque()
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        INF = 2147483647
+
+        for i in range(rows):
+            for j in range(cols):
+                if rooms[i][j] == 0:
+                    queue.append((i, j))
+
+        level = 1  # neighbors of gates are 1 step away
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                curr_r, curr_c = queue.popleft()
+                for nr, nc in directions:
+                    next_r, next_c = curr_r + nr, curr_c + nc
+                    if (
+                        0 <= next_r < rows
+                        and 0 <= next_c < cols
+                        and rooms[next_r][next_c]== INF  # only walkable, unvisited rooms
+                    ):
+                        rooms[next_r][next_c] = level
+                        queue.append((next_r, next_c))
+            level += 1
