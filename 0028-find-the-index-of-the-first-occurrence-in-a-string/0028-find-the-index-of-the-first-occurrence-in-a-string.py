26481d9a7013a4e95029d818218f37abeeb0b429
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-02 22:00:54 +0200

    Time: 2 ms (22.57%), Space: 19.5 MB (8.39%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -3,7 +3,8 @@ class Solution:
     # Refer: Namaste DSA (Two Pointers and Sliding Window)
     # Not in NC250 but leads to KMP algorithm
     def strStr(self, haystack: str, needle: str) -> int:
-        # Approach 1: Brute force, Time:  O(n*m)
+        """
+        # Approach 1: Brute force, Time: O(n*m)
         n = len(haystack)
         m = len(needle)
 
@@ -14,4 +15,35 @@ class Solution:
                     break
             else:
                 return i
+        return -1
+        """
+        # Approach 2: KMP, Time: O(n+m) = O(n), Space: O(m)
+        n = len(haystack)
+        m = len(needle)
+        lps = [0] * m
+        i = 0
+        j = 1
+        while j < m:
+            if needle[i] == needle[j]:
+                i += 1
+                lps[j] = i
+                j += 1
+            else:
+                if i == 0:
+                    lps[j] = 0
+                    j += 1
+                else:
+                    i = lps[i - 1]
+        i = j = 0
+        while i < n:
+            if haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                if j == 0:
+                    i += 1
+                else:
+                    j = lps[j - 1]
+            if j == m:
+                return i - m
         return -1
