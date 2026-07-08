b0fd7514b0f7c92d833a32ed7e82a56e6568b96a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-08 20:38:08 +0200

    Time: 23 ms (12.87%), Space: 19.4 MB (12.75%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.py
+++ 0686-repeated-string-match/0686-repeated-string-match.py
@@ -2,13 +2,9 @@ class Solution:
     # Date Solved: 3 July 2026, Friday
     # Refer: Namaste DSA, not in NCA All
     def repeatedStringMatch(self, a: str, b: str) -> int:
+        """
         # Approach 1: Brute Force
         # Time: O(n × (m + n)) with m = len(a), n = len(b), Space: O(m + n)
-        """
-        The while loop repeats a until text is at least as long as b, which takes about n/m iterations, building text up to length O(m + n).
-Each b in text check is a substring search. Worst-case (naive) substring matching costs O(len(text) × len(b)), i.e. O((m+n) × n).
-Since this check happens (up to) twice on a string of size O(m+n), the dominant cost is O(n × (m+n)).
-"""
         count = 1
         text = a
 
@@ -26,3 +22,52 @@ Since this check happens (up to) twice on a string of size O(m+n), the dominant 
             return count
 
         return -1
+        """
+        # Approach 2: Rabin Karp
+        # # Time: O(m + n) in average with m = len(a), n = len(b), Space: O(1)
+        count = 1
+        text = a
+
+        while len(text) < len(b):
+            text += a
+            count += 1
+
+        if self.rabinKarp(text, b):
+            return count
+
+        text += a
+        count += 1
+
+        if self.rabinKarp(text, b):
+            return count
+
+        return -1
+
+    def rabinKarp(self, text, pattern):
+        base = 256
+        mod = int(1e9 + 7)
+
+        n = len(text)
+        m = len(pattern)
+
+        patternHash = 0
+        windowHash = 0
+
+        for i in range(m):
+            patternHash = (patternHash * base + ord(pattern[i])) % mod
+            windowHash = (windowHash * base + ord(text[i])) % mod
+
+        power = 1
+        for _ in range(m - 1):
+            power = (power * base) % mod
+
+        for i in range(n - m + 1):
+            if patternHash == windowHash:
+                if text[i : i + m] == pattern:
+                    return True
+
+            if i < n - m:
+                windowHash = (windowHash - (power * ord(text[i])) % mod + mod) % mod
+                windowHash = (windowHash * base + ord(text[i + m])) % mod
+
+        return False
