1a7b8b4f9fc9cbb89f689f739751ea79d3355458
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-12 13:39:11 +0200

    Time: 55 ms (29.3%), Space: 37.6 MB (60.61%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,6 +1,10 @@
 class Solution:
     # Date Solved: 12 July 2026, Sunday, POTD
+    # Time: O(nlogn), Space: O(n)
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        # Both mine and codestorywithMIK's solution have same Time and Space
+        """
+        # My solution
         val_idx_map = {}
         n = len(arr)
         result = [0] * n
@@ -17,4 +21,20 @@ class Solution:
                 output_indices = val_idx_map.pop(val)
                 for i in range(len(output_indices)):
                     result[output_indices[i]] = current_rank
+        return result
+        """
+        # codestorywithMIK solution
+        temp = sorted(arr)
+        n = len(arr)
+        result = []
+        
+        rank_map = {}
+        rank = 1
+        for i, val in enumerate(temp):
+            if i > 0 and val > temp[i - 1]:
+                rank += 1
+            rank_map[val] = rank
+
+        for val in arr:
+            result.append(rank_map[val])
         return result
