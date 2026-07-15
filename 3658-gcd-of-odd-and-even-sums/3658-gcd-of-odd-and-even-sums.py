69026d98779f433084ab58303a80b67e64ab061a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-15 12:06:55 +0200

    Time: 0 ms (100%), Space: 19.3 MB (53.03%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,19 @@
+from math import gcd
+
+
+class Solution:
+    # Date Solved: 15 July 2026, Wednesday, POTD
+    # Refer: codestorywithMIK for the derivation of the formula
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        """
+        # Approach 1: Simple Maths
+        # Time : O(log(n)), Space : O(1)
+        sum_odd = n * n
+        sum_even = n * (n + 1)
+        return gcd(sum_odd, sum_even)
+        """
+        # Approach 2: Simple Maths and Constant Time
+        # Time : O(1), Space : O(1)
+        # sumOdd = n*n, sumEven = n*(n+1)
+        # gcd(n*n, n*(n+1)) = n * gcd(n, n+1) = n * 1 = n
+        return n
