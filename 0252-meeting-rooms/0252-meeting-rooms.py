f71a88704954925df5254e9e798ba41949f4abbc
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-15 13:09:44 +0200

    Time: 5 ms (16.22%), Space: 21.2 MB (76.8%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -2,10 +2,12 @@ class Solution:
     # Date Solved: 15 July 2026, Wednesday
     # Blind 75
     # Refer: NC Ashish, solved on my own after practicing other "Intervals" problem.
+    # No codestorywithMIK and NeetCode's approach similar to mine.
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        # Time: O(nlogn), Space: O(1) ignoring extra space used by sorting
         if len(intervals) == 0:
             return True
-        intervals.sort()
+        intervals.sort(key=lambda x: x[0])
         prev_end = intervals[0][1]
         for i in range(1, len(intervals)):
             start, end = intervals[i][0], intervals[i][1]
