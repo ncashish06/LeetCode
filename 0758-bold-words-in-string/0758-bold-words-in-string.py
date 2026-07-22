b15a7af6e55e9ed5094d5990fc91357f4366c8a3
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-22 16:03:16 +0200

    Time: 4 ms (91.67%), Space: 19.2 MB (94.44%) - LeetHub

---
+++ 0758-bold-words-in-string/0758-bold-words-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    # Date Solved: 22 July 2026, Wednesday, Weekly Premium W4
+    # Refer: NeetCode solution for LC. 616 "Add Bold Tag in String"
+    # Same as LC. 616 "Add Bold Tag in String" which is in NC All.
+    def boldWords(self, words: List[str], s: str) -> str:
+        n = len(s)
+        bold = [False] * n  # bold[i] = True if s[i] should be bolded
+
+        # Mark every index covered by any occurrence of any word
+        for word in words:
+            start = s.find(word)
+            while start != -1:
+                for i in range(start, start + len(word)):
+                    bold[i] = True
+                start = s.find(word, start + 1)  # find next overlapping occurrence
+
+        open_tag = "<b>"
+        close_tag = "</b>"
+        ans = []
+
+        # Walk through s, inserting a tag only at the boundary of a bold run
+        # (this is what keeps the tag count minimal / merges overlapping words)
+        for i in range(n):
+            # start of a bold run: current char is bold AND (first char, or previous char wasn't bold)
+            if bold[i] and (i == 0 or not bold[i - 1]):
+                ans.append(open_tag)
+
+            ans.append(s[i])
+
+            # end of a bold run: current char is bold AND (last char, or next char isn't bold)
+            if bold[i] and (i == n - 1 or not bold[i + 1]):
+                ans.append(close_tag)
+
+        return "".join(ans)
