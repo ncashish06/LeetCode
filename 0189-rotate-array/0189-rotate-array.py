81f71668b1c3a8debdfcbec0d8c51770e395241a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-23 18:21:19 +0200

    Time: 6 ms (56.85%), Space: 26.5 MB (70.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,15 +1,16 @@
 class Solution:
     # Date Solved: 8 May 2026, Friday
+    # NC250
     """
-    Refer: Part of NeetCode 250 solution
     Approach: 3 Reversals
         1. Reverse entire array
         2. Reverse first k elements
         3. Reverse remaining elements
 
     Related Problems:
-    - LC48  (Rotate Image):  Uses the same layer-by-layer ring traversal idea, but rotates a 2D matrix 90°.
-    - LC1914 (Cyclically Rotating a Grid): Extracts each ring into a 1D array, then applies this exact 3-reversal logic.
+    - LC.48 Rotate Image (Blind 75):  Uses the same layer-by-layer ring traversal idea, but rotates a 2D matrix 90°.
+    - LC.1914 Cyclically Rotating a Grid: Extracts each ring into a 1D array, then applies this exact 3-reversal logic.
+    - LC. 1260 Shift 2D Grid (NC All): Refer codestorywithMIK's video.
     """
 
     def rotate(self, nums: List[int], k: int) -> None:
