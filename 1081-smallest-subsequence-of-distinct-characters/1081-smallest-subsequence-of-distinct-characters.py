e73184cccfaccae3186fd3df260ee6d7bf3fa587
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-23 15:04:21 +0200

    Time: 3 ms (38.92%), Space: 19.3 MB (42.45%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,63 @@
+class Solution:
+    # Date Solved: 23 July 2026, Thursday
+    # POTD of 19 July 2026, Sunday (Unsolved due to Bari trip)
+    # Refer: codestorywithMIK's "LC. 316 Remove Duplicate Letters" video which is exactly same.
+    def smallestSubsequence(self, s: str) -> str:
+        # Approach-1 (Using string as a stack)
+        # Time : O(n), Space : O(1)
+        n = len(s)
+        result = []
+
+        taken = [False] * 26  # O(1) space
+        lastIndex = [0] * 26  # O(1) space
+
+        for i in range(n):
+            ch = s[i]
+            lastIndex[ord(ch) - ord("a")] = i
+
+        for i in range(n):
+            idx = ord(s[i]) - ord("a")
+
+            if taken[idx]:
+                continue
+
+            while (
+                result
+                and s[i] < result[-1]
+                and lastIndex[ord(result[-1]) - ord("a")] > i
+            ):
+                taken[ord(result[-1]) - ord("a")] = False
+                result.pop()
+
+            result.append(s[i])
+            taken[idx] = True
+
+        return "".join(result)
+
+        """
+        # Approach-2 (Using stack)
+        # Time : O(n), Space: O(n) stack
+        n = len(s)
+        st = []  # using a list as a stack
+
+        taken = [False] * 26
+        lastIndex = [0] * 26
+
+        for i in range(n):
+            ch = s[i]
+            lastIndex[ord(ch) - ord("a")] = i
+
+        for i in range(n):
+            idx = ord(s[i]) - ord("a")
+
+            if taken[idx]:
+                continue
+
+            while st and s[i] < st[-1] and lastIndex[ord(st[-1]) - ord("a")] > i:
+                taken[ord(st.pop()) - ord("a")] = False
+
+            st.append(s[i])
+            taken[idx] = True
+
+        return "".join(st)
+        """
