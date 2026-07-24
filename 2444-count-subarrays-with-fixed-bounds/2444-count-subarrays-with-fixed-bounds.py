570845efdc0cc73c3658654447b0f61115b04c5e
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-24 14:39:50 +0200

    Time: 98 ms (63.12%), Space: 31.5 MB (22.59%) - LeetHub

---
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,46 @@
+class Solution:
+    # Date Solved: 24 July 2026, Friday
+    # codestorywithMIK says Asked yesterday for Adobe OA.
+    # Refer: codestorywithMIK
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        """
+        # Approach-1: Brute Force - Find all subarrays and check if min is minK and max is maxK
+        # Time: O(n^2), Space: O(1)
+        n = len(nums)
+        count = 0
+
+        for start in range(n):
+            currentMin = nums[start]
+            currentMax = nums[start]
+
+            for end in range(start, n):
+                currentMin = min(currentMin, nums[end])
+                currentMax = max(currentMax, nums[end])
+
+                if currentMin == minK and currentMax == maxK:
+                    count += 1
+
+        return count
+        """
+        # Approach-2 : Sliding Window
+        # Time: O(n), Space: O(1)
+        ans = 0
+
+        minPosition = -1
+        maxPosition = -1
+        culpritIdx = -1
+
+        for i in range(len(nums)):
+            if nums[i] < minK or nums[i] > maxK:
+                culpritIdx = i
+
+            if nums[i] == minK:
+                minPosition = i
+            if nums[i] == maxK:
+                maxPosition = i
+
+            count = min(maxPosition, minPosition) - culpritIdx
+
+            ans += 0 if count <= 0 else count
+
+        return ans
