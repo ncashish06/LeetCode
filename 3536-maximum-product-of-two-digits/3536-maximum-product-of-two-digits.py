4d2ee1d6d7f1d3bfe4fbb1237aaa40751d55cc0d
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-25 09:59:10 +0200

    Time: 0 ms (100%), Space: 19.4 MB (22.61%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -2,6 +2,9 @@ class Solution:
     # Date Solved: 25 July 2026, Saturday, POTD
     # Refer: NC Ashish
     def maxProduct(self, n: int) -> int:
+        """
+        # Approach 1: Solved on my own
+        # Time: O(dlogd) where d = number of digits in n, Space: O(d)
         digits = []
         while n:
             digit = n % 10
@@ -9,3 +12,15 @@ class Solution:
             digits.append(digit)
         digits.sort(reverse=True)
         return digits[0] * digits[1]
+        """
+        # Approach 2: Optimal
+        # Time: O(logn), Space: O(1)
+        max_digit, second_max_digit = 0, 0
+        while n:
+            digit = n % 10
+            if digit > max_digit:
+                max_digit, second_max_digit = digit, max_digit
+            elif digit > second_max_digit:
+                second_max_digit = digit
+            n //= 10
+        return max_digit * second_max_digit
