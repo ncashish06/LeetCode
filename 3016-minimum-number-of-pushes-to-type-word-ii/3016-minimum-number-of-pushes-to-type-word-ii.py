e0051c22cee3ba80ee33dc834e9b979f432835d3
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-07-30 11:52:42 +0200

    Time: 188 ms (23.31%), Space: 19.9 MB (93.58%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Date Solved: 30 July 2026, Thursday
+    # Refer: codestorywithMIK. Below approach works for both this and "LC. 3014 Minimum Number of Pushes to Type Word I (Easy)"
+    def minimumPushes(self, word: str) -> int:
+        # Time: O(n) + O(26log26) for sorting, Space: O(1)
+        mp = [0] * 26
+        # count frequency (letters can repeat in Part II unlike in Part I)
+        for ch in word:
+            mp[ord(ch) - ord("a")] += 1
+
+        mp.sort(reverse=True)  # descending order of frequency
+
+        ans = 0
+        for i in range(26):
+            freq = mp[i]
+            pressed_key = (i // 8) + 1
+            ans += freq * pressed_key
+
+        return ans
