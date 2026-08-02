dcbd10ed3c36cbfa6e89688d7abe6aae158f56d2
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-02 11:49:56 +0200

    Time: 2 ms (60.64%), Space: 19.5 MB (42.75%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,10 +1,11 @@
 class Solution:
     # Date Solved: 1 August 2026, Saturday, POTD
     # Refer: codestorywithMIK
+    # Game Strategy: When it is your turn, do your best and choose maximum. Since opponent also plays optimally, expect the worst from result after opponent's turn. So it is alternating max->min->max->min... structure of the recursion, i.e., classic minimax: maximize on your turn, minimize (from your perspective) on the opponent's turn
     def predictTheWinner(self, nums: List[int]) -> bool:
-        """
-        # Approach 1: Recursion + Memo - I
-        # Time: O(n^2), Space: O(n^2)
+        # Approach: Recursion + Memo - I
+        # Time: O(n^2) with memo else O(2^n)
+        # Space: O(n^2) with memo else O(2^n)
         n = len(nums)
         memo = {}
 
@@ -27,23 +28,3 @@ class Solution:
         player2 = total - player1
 
         return player1 >= player2
-        """
-        # Approach 2: Using diff of score between players
-        # Recursion + Memo - II
-        # Time: O(n^2), Space: O(n^2)
-        n = len(nums)
-        memo = {}
-
-        def maxDiff(l: int, r: int) -> int:
-            if l == r:
-                return nums[l]
-            if (l, r) in memo:
-                return memo[(l, r)]
-
-            take_left = nums[l] - maxDiff(l + 1, r)
-            take_right = nums[r] - maxDiff(l, r - 1)
-
-            memo[(l, r)] = max(take_left, take_right)
-            return memo[(l, r)]
-
-        return maxDiff(0, n - 1) >= 0
