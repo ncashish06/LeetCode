10b1086e1cb9ffab79af01386d22ccbfe0368dde
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-07 11:23:39 +0200

    Time: 23 ms (9.84%), Space: 24.1 MB (9.03%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,6 @@
 class Solution:
     # Date Solved: 25 April 2026, Saturday
+    # Blind 75
     # Refer: Neetcode 150 Course on freeCodeCamp.org YouTube
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = {}  # res = defaultdict(list)
