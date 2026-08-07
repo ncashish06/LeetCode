ac50517a504740ede4e2961f4f82036bfe6f6485
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-07 11:20:38 +0200

    Time: 3 ms (17.13%), Space: 19.3 MB (31.87%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.py
@@ -1,14 +1,18 @@
 class Solution:
     # Date Solved: 6 August 2026, Thursday, POTD
+    # Time: O(log(n))
     def smallestNumber(self, n: int, t: int) -> int:
-        def digit_product(x: int) -> int:
+        def digit_product(num):
             product = 1
-            while x > 0:
-                product *= x % 10
-                x //= 10
+            while num:  # Time: O(digits) = O(log10(num))
+                product *= num % 10
+                if product == 0:
+                    return 0
+                num //= 10
             return product
 
-        num = n
-        while digit_product(num) % t != 0:
-            num += 1
-        return num
+        for i in range(n, n + 10):  # Time: O(10)
+            if digit_product(i) % t == 0:  # Time: O(digits) = O(log10(num))
+                return i
+        # Not needed for correctness of the algorithm, but good practice to leave in for type-safety.
+        return -1
