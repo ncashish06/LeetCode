3895f7960e1106c76cb8bc505e51d95cd47487aa
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-12 07:44:12 +0200

    Time: 267 ms (37.72%), Space: 35.2 MB (94.91%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -0,0 +1,27 @@
+class Solution:
+    # Date Solved: 12 August 2026, Wednesday, POTD
+    # Refer: codestorywithMIK, NC's approach is same
+    # In NC All
+    # Approach: Classic Khandani Sliding Window Template
+    # Time: O(n) as each element is visited atmost twice = O(2n), don't get confused because of nested loops
+    # Space: O(n)
+    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        mp = defaultdict(int)
+
+        i = 0
+        j = 0
+        result = 0
+
+        while j < n:
+
+            mp[nums[j]] += 1
+
+            while i < j and mp[nums[j]] > k:
+                mp[nums[i]] -= 1
+                i += 1
+
+            result = max(result, j - i + 1)
+            j += 1
+
+        return result
