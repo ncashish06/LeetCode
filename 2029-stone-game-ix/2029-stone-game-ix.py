b9390480a0c36e0ae26b3f3b77fd69b82498195a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-16 23:24:21 -0700

    Time: 63 ms (25.93%), Space: 30.6 MB (60%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.py
+++ 2029-stone-game-ix/2029-stone-game-ix.py
@@ -1,16 +1,22 @@
 class Solution:
     # Date Solved: 16 August 2026, Sunday, POTD
     # Refer: codestorywithMIK
+    # Approach: Modular Arithmetic and Count Comparison
     # Time: O(n), Space: O(1)
     def stoneGameIX(self, stones: List[int]) -> bool:
-        cnt0 = cnt1 = cnt2 = 0
-        for val in stones:
-            if (typ := val % 3) == 0:
-                cnt0 += 1
-            elif typ == 1:
-                cnt1 += 1
+        c0 = 0
+        c1 = 0
+        c2 = 0
+
+        for stone in stones:
+            if stone % 3 == 0:
+                c0 += 1
+            elif stone % 3 == 1:
+                c1 += 1
             else:
-                cnt2 += 1
-        if cnt0 % 2 == 0:
-            return cnt1 >= 1 and cnt2 >= 1
-        return cnt1 - cnt2 > 2 or cnt2 - cnt1 > 2
+                c2 += 1
+
+        if c0 % 2 == 0:  # even
+            return (c1 >= 1 and c2 >= 1) and (c2 >= c1 or c1 >= c2)
+
+        return abs(c1 - c2) >= 3
