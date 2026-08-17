8af0d527a967a998ea941a296c86b626e254e581
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-16 23:36:48 -0700

    Time: 8748 ms (19.6%), Space: 74.4 MB (33.33%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.py
+++ 1563-stone-game-v/1563-stone-game-v.py
@@ -7,8 +7,6 @@ class Solution:
         n = len(stoneValue)
         t = [[-1] * 501 for _ in range(501)]
 
-        # sys.setrecursionlimit(10000)
-
         def solve(l, r, cumSum):
             if l >= r:
                 return 0  # Zero score. No further division possible
@@ -41,3 +39,38 @@ class Solution:
             cumSum[i] = cumSum[i - 1] + stoneValue[i]
 
         return solve(0, n - 1, cumSum)
+        """
+        # Approach-2: Bottom Up (Tabulation)
+        # Time: O(n^3), Space: O(n^2)
+        n = len(stoneValue)
+
+        cumSum = [0] * n
+        cumSum[0] = stoneValue[0]
+        for i in range(1, n):
+            cumSum[i] = cumSum[i - 1] + stoneValue[i]
+
+        t = [[0] * (n + 1) for _ in range(n + 1)]
+
+        for l in range(n - 1, -1, -1):
+            for r in range(l + 1, n):
+                # l...r
+                score = 0
+                for mid in range(l, r):
+                    leftSum = cumSum[mid] - (
+                        cumSum[l - 1] if l - 1 >= 0 else 0
+                    )  # [l..mid]
+                    rightSum = cumSum[r] - cumSum[mid]  # mid+1, r
+
+                    if leftSum < rightSum:
+                        score = max(score, leftSum + t[l][mid])
+                    elif leftSum > rightSum:
+                        score = max(score, rightSum + t[mid + 1][r])
+                    else:
+                        score = max(
+                            score, leftSum + t[l][mid], rightSum + t[mid + 1][r]
+                        )
+
+                t[l][r] = score
+
+        return t[0][n - 1]
+        """
