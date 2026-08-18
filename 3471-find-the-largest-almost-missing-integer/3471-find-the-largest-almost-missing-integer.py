ae36858dc4d7901662a26ff6e566fada28087d7a
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-18 00:06:13 -0700

    Time: 0 ms (100%), Space: 19.3 MB (75.41%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.py
@@ -1,9 +1,11 @@
 class Solution:
     # Date Solved: 18 August 2026, Tuesday, POTD
     # Refer: codestorywithMIK
-    # Approach: Sliding Window
-    # Go to each subarray of size k (window of size k) and mark which element came in that subarray. In the end, check which element came in exactly 1 subarray
     def largestInteger(self, nums: List[int], k: int) -> int:
+        """
+        # Approach-1: Sliding Window
+        # Time: O(n*k), Space: O(n)
+        # Go to each subarray of size k (window of size k) and mark which element came in that subarray. In the end, check which element came in exactly 1 subarray
         n = len(nums)
 
         i = 0
@@ -36,3 +38,39 @@ class Solution:
                 result = num
 
         return result
+        """
+        # Approach-2: Simple Observation
+        # Time: O(n), Space: O(1)
+        n = len(nums)
+
+        # case 1 : When k == n
+        if k == n:  # only one subarray possible. Select the largest element
+            return max(nums)
+
+        # case 2 : When k == 1
+        freq = [0] * 51
+        for num in nums:
+            freq[num] += 1
+
+        # every index is one subarray. Find largest element which appears only once
+        if k == 1:
+            for num in range(50, -1, -1):
+                if freq[num] == 1:
+                    return num
+            return -1
+
+        # case 3 : 1 < k < n
+        # Notice that only the first and last element are going to be the
+        # ones which will appear only in one subarray. Others will be part
+        # of more than one subarray always
+
+        maxResult = -1
+        # So, if nums[0] appeared only at index 0, then it appeared only in one subarray
+        if freq[nums[0]] == 1:
+            maxResult = max(maxResult, nums[0])
+
+        # Same, if nums[n-1] appeared only at index n-1, then it appeared only in one subarray
+        if freq[nums[n - 1]] == 1:
+            maxResult = max(maxResult, nums[n - 1])
+
+        return maxResult
