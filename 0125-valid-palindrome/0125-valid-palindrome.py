3fcd085535e271fb974db8ccf72f501a2a41bfed
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-19 22:11:48 -0700

    Time: 23 ms (5.43%), Space: 19.6 MB (52.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,24 @@
 class Solution:
+    # Date Solved: 19 August 2026, Wednesday
+    # Blind 75
+    # Refer: NeetCode
+    # Time: O(n), Space: O(1)
     def isPalindrome(self, s: str) -> bool:
-        i, j = 0, len(s) - 1
-        while i < j:
-            if not s[i].isalnum():
-                i += 1
-            elif not s[j].isalnum():
-                j -= 1
-            elif s[i].lower() == s[j].lower():
-                i += 1
-                j -= 1
-            else:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
                 return False
+            l, r = l + 1, r - 1
         return True
+
+    def alphaNum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
