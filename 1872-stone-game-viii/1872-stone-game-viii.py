9f47d3bd5f6274ab16845c7b9a3daff2349f711c
Ashish Nadadur Chakravarthi <66215582+ncashish06@users.noreply.github.com>
2026-08-24 22:04:55 -0700

    Time: 869 ms (13.53%), Space: 131.4 MB (11.76%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -3,10 +3,39 @@ class Solution:
     # Refer: codestorywithMIK
     # Time: O(n), Space: O(n)
     def stoneGameVIII(self, stones: List[int]) -> int:
+        # Approach-1: Recursion + Memo
+        # Time: O(n), Space: O(n)
         n = len(stones)
-        pre = list(accumulate(stones))
-        f = [0] * n
-        f[n - 1] = pre[n - 1]
+        prefixSum = list(accumulate(stones))
+        t = {}
+
+        def solve(i: int) -> int:
+            if i == n - 1:
+                return prefixSum[n - 1]
+
+            if i in t:
+                return t[i]
+
+            take = prefixSum[i] - solve(i + 1)
+            skip = solve(i + 1)
+
+            t[i] = max(take, skip)
+            return t[i]
+
+        return solve(1)  # Alice turns first
+        """
+        # Approach-2: Bottom-up
+        # Time: O(n), Space: O(n)
+        n = len(stones)
+        prefixSum = list(accumulate(stones))
+
+        t = [0] * n
+        t[n - 1] = prefixSum[n - 1]  # base case: solve(n-1)
+
         for i in range(n - 2, 0, -1):
-            f[i] = max(f[i + 1], pre[i] - f[i + 1])
-        return f[1]
+            take = prefixSum[i] - t[i + 1]
+            skip = t[i + 1]
+            t[i] = max(take, skip)
+
+        return t[1]  # == solve(1)
+        """
